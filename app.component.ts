61d271e508241ac4d91d8cdcfba66eb28c1aa0d9
Snigdha Srivastava <35126671+SnigdhaSrivastava@users.noreply.github.com>
2020-04-29 16:25:56 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as d3 from 'd3';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'assig2';
   serverData : JSON;
@@ -28,6 +32,7 @@ export class AppComponent {
       console.log(this.serverData);
       console.log(this.serverData[0]);
       console.log(this.serverData[0][1]);
+      console.log(this.serverData[0][2]);
 
       this.svgnodes();
 
@@ -35,27 +40,33 @@ export class AppComponent {
     })
   }
 
+
+  //function to create nodes using svg and d3
   svgnodes() {
     
-    
+    //creating the svg container
     var svgContainer = d3.select("body").append("svg")
                                      .attr("width", 1000)
                                      .attr("height", 600)
                                      .style("border", "1px solid black");
 
 
+    //creating nodes
     for (let i = 0; i < this.count; i++){
-       console.log(i);
-       let j=0;
-      
-            console.log(i,j);
 
-            var rectangle = svgContainer.append("rect")
-            .attr("x", this.serverData[i][j])
-            .attr("y", this.serverData[i][j+1])
-            .attr("width", 50)
-            .attr("height", 50)
-            .style("fill","green");
+      console.log(i);
+      let j=0;
+      
+      console.log(i,j);
+
+      var rectangle = svgContainer.append("rect")
+      .attr("x", this.serverData[i][j])
+      .attr("y", this.serverData[i][j+1])
+      .attr("width", 50)
+      .attr("height", 50)
+      .style("fill","green")
+      .append("svg:title")             //tooltip hovering
+      .text(this.serverData[i][2]);
 
                                       
      }
